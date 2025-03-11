15cb88f32ad12d980aa84746dfeb5ad215dd11a9
Umerashaik281 <umeerashaik281@gmail.com>
2025-03-11 22:37:27 +0530

    Add files via upload

---
+++ PrimitiveLocal.java
@@ -0,0 +1,26 @@
+package primitiveLocal;
+
+public class PrimitiveLocal {
+	public static void main(String [] args) {
+		byte b1=10;
+		short s1=25;
+		int i1=30;
+		long l1=10000L;
+		float f1=15.5f;
+		char ch='u';
+		boolean bool=false;
+		String str1="hello, good morning!";
+		System.out.println(b1);
+		System.out.println(s1);
+		System.out.println(i1);
+		System.out.println(l1);
+		System.out.println(f1);
+		System.out.println(ch);
+		System.out.println(bool);
+		System.out.println(str1);
+		
+		
+		
+	}
+
+}
---
+++ PrimitiveStatic.java
@@ -0,0 +1,24 @@
+package primitiveStatic;
+
+public class PrimitiveStatic {
+	public static void main(String [] args) {
+		byte b1 = 0;
+		short s1 = 0;
+		int i1 = 0;
+		long l1 = 0;
+		float f1 = 0;
+		char ch = 0;
+		boolean bool = false;
+		String str1 = null;
+		System.out.println(b1);
+		System.out.println(s1);
+		System.out.println(i1);
+		System.out.println(l1);
+		System.out.println(f1);
+		System.out.println(ch);
+		System.out.println(bool);
+		System.out.println(str1);
+		
+	}
+
+}
